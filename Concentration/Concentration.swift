6330c119bea2e5cad10eb921951d592761cddd01
Jingmin GUAN <jguan@groupama-ra.fr>
2019-11-25 19:37:44 +0100

    [dev]ConcentrationGame1

---
+++ Concentration/Concentration.swift
@@ -0,0 +1,50 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Jingmin Guan on 25/11/2019.
+//  Copyright © 2019 Jingmin Guan. All rights reserved.
+//
+
+import Foundation
+
+class Concentration{
+    
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int){
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                //either no cards of 2 cards are face up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+            
+        }
+        if cards[index].isFaceUp {
+            cards[index].isFaceUp = false
+        } else {
+            cards[index].isFaceUp = true
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int){
+        for _ in 1...numberOfPairsOfCards{
+            let card = Card()
+            cards += [card, card]
+        }
+        //TODO: Shuffle the cards
+    }
+}
